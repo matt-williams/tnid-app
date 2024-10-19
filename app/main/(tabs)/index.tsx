a3afc81d2a94e2e321ff3964b0c6894739e9ce70
Matt Williams <matwilliams@hotmail.com>
2024-10-19 11:13:37 +0100

    Very primitive login interface

--- app/main/(tabs)/index.tsx
+++ app/main/(tabs)/index.tsx
@@ -1,48 +1,15 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Platform } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedTextInput } from '@/components/ThemedTextInput';
 import { ThemedView } from '@/components/ThemedView';
-import ThemedButton from '@/components/ThemedButton';
 
 import React, { useState } from "react";
 
-export default function LoginScreen() {
+export default function HomeScreen() {
   const [phoneNumber, setPhoneNumber] = useState("");
-
-  const getMovies = async () => {
-    alert(phoneNumber);
-    try {
-      const url = 'https://api.staging.v2.tnid.com/auth/create_user_otp';
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json' // Set the content type to JSON
-        },
-        body: JSON.stringify({'telephone_number': phoneNumber})
-      });
-      const json = await response.json();
-      setData(json.movies);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Login</Text>
-      <ThemedTextInput
-        onChangeText={setPhoneNumber}
-        value={phoneNumber}
-        placeholder="Phone number"
-        keyboardType="phone-pad"
-      />
-      <ThemedButton label="Log in" onPress={getMovies} />
-    </View>
-/*
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
@@ -63,21 +30,10 @@ export default function LoginScreen() {
         />
       </ThemedView>
     </ParallaxScrollView>
-*/
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-/*
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -94,5 +50,4 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
-*/
 });
